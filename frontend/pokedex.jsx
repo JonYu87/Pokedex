439146efc30abf3dc2bf6b4bdde7da7d4f5c893a
jon <jonathanyusf@gmail.com>
2021-09-17 18:19:14 -0700

    Add and test api_util for fetching pokemon, it works

--- frontend/pokedex.jsx
+++ frontend/pokedex.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM  from "react-dom";
+import {fetchAllPokemon} from "./util/api_util";
 
 document.addEventListener("DOMContentLoaded", function(){
   const root = document.getElementById("root");
   ReactDOM.render(<h1>Hello Poke-World</h1>, root);
+
+  window.fetchAllPokemon = fetchAllPokemon;
+
 });
