02084cb2f779447a75d3708dfaabc03617e6901c
jon <jonathanyusf@gmail.com>
2021-09-20 17:37:35 -0700

    Test and add receiveAllPokemon action

--- frontend/pokedex.jsx
+++ frontend/pokedex.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import ReactDOM  from "react-dom";
-import {fetchAllPokemon} from "./util/api_util";
+import ReactDOM from "react-dom";
+import { fetchAllPokemon } from "./util/api_util";
+import { receiveAllPokemon } from "./actions/pokemon_actions";
 
-document.addEventListener("DOMContentLoaded", function(){
+
+document.addEventListener("DOMContentLoaded", function () {
   const root = document.getElementById("root");
   ReactDOM.render(<h1>Hello Poke-World</h1>, root);
 
   window.fetchAllPokemon = fetchAllPokemon;
-
+  window.receiveAllPokemon = receiveAllPokemon;
 });
