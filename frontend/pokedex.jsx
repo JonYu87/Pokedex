978c07043c9b63daa608d54959637fadf142f8c9
jon <jonathanyusf@gmail.com>
2021-09-20 18:13:52 -0700

    Add and test store on console

--- frontend/pokedex.jsx
+++ frontend/pokedex.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { fetchAllPokemon } from "./util/api_util";
 import { receiveAllPokemon } from "./actions/pokemon_actions";
+import { configureStore } from "./store/store";
 
 
 document.addEventListener("DOMContentLoaded", function () {
   const root = document.getElementById("root");
   ReactDOM.render(<h1>Hello Poke-World</h1>, root);
 
+  const store = configureStore();
+
+  window.getState = store.getState;
+  window.dispatch = store.dispatch
   window.fetchAllPokemon = fetchAllPokemon;
   window.receiveAllPokemon = receiveAllPokemon;
 });
