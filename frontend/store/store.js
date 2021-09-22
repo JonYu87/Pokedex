0d1c20a30a6f0bc286e1ba6150a5ed2ebf0da3e1
jon <jonathanyusf@gmail.com>
2021-09-21 18:07:13 -0700

    Add thunk action to pokemon actions

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware } from "redux";
 import { logger } from "redux-logger";
+import thunk from "redux-thunk";
 import { rootReducer } from "../reducers/root_reducer";
 
-export const configureStore = () => createStore(rootReducer, applyMiddleware(logger));
+export const configureStore = () => createStore(rootReducer, applyMiddleware(thunk, logger));
